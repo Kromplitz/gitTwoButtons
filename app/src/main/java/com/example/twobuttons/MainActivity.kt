996b2ad8fca4e0deac4676a877628ec6a86765d6
Oles <Kromplias@iMac-5k-Demo.Dlink>
2024-01-02 12:52:24 +0200

    homework done

---
+++ app/src/main/java/com/example/twobuttons/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.twobuttons
+
+import android.app.Activity
+import android.graphics.Insets.add
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : Activity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+        var res =0
+        val addButton:Button = findViewById(R.id.addButton)
+        addButton.setOnClickListener{
+
+            Toast.makeText(this, "${res++}", Toast.LENGTH_SHORT).show()
+
+        }
+        val subButton:Button = findViewById(R.id.subButton)
+        subButton.setOnClickListener{
+
+            if (res<1){Toast.makeText(this, "Помилка", Toast.LENGTH_SHORT).show()
+            }
+            else {Toast.makeText(this, "${res--}", Toast.LENGTH_SHORT).show()}
+        }
+
+
+    }
+
+}
